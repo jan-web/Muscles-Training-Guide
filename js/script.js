67d19c52b7856c909321e96709de8a77c1a222d5
Yan Shatskiy <writemetolinkedin@git.io>
2021-01-19 11:48:24 +0100

    The unnecessary code from css hes been delited

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
 const bodyesArea = document.querySelector('.male-muscles');
 const descriptions = document.querySelectorAll('.description');
 
-let activePart = 'trapezius';
 bodyesArea.addEventListener('click', (e)=>{
 
 	const part = e.target.dataset.part;
 	if(part) {
 		descriptions.forEach(element => {
 			element.classList.remove('active');
-			
+
 			if(element.classList.contains(part)){
 				console.log('part');
 				element.classList.remove('noactive');
 				element.classList.add('active');
-
 			}
-
 		});
-		// descriptions.some(element => { element.classList.contains('active')
-
-		// }).classList.remove('active');
-
-		activePart = part;
 	}
-})
+});
